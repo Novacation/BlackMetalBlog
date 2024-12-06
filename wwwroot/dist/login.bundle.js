cfaa927c2096352d44190053b3edb0e7833c7082
NoVacation <andersoncelestino.n@gmail.com>
2024-12-06 17:38:19 -0300

    Updates

---
+++ wwwroot/dist/login.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./wwwroot/js/Auth/login.js":
+/*!**********************************!*\
+  !*** ./wwwroot/js/Auth/login.js ***!
+  \**********************************/
+/***/ (() => {
+
+eval("var millisecondsToSeconds = function millisecondsToSeconds(seconds) {\n  return seconds * 1000;\n};\nwindow.onload = function () {\n  var errorMessageDiv = document.getElementById(\"invalidCredentialsError\");\n  if (errorMessageDiv) {\n    setTimeout(function () {\n      errorMessageDiv.style.display = \"none\";\n    }, millisecondsToSeconds(3)); // 3000 milliseconds = 3 seconds\n  }\n};\n\n//# sourceURL=webpack://blackmetalblog/./wwwroot/js/Auth/login.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./wwwroot/js/Auth/login.js"]();
+/******/ 	
+/******/ })()
+;
