392bf15c31f317c57109db3842d1c4a71f085270
NoVacation <andersoncelestino.n@gmail.com>
2024-12-06 17:36:56 -0300

    Updates

---
+++ wwwroot/js/Auth/login.js
@@ -0,0 +1,11 @@
+﻿const millisecondsToSeconds = (seconds) => seconds * 1000;
+
+
+window.onload = function () {
+    let errorMessageDiv = document.getElementById("invalidCredentialsError");
+    if (errorMessageDiv) {
+        setTimeout(function () {
+            errorMessageDiv.style.display = "none";
+        }, millisecondsToSeconds(3)); // 3000 milliseconds = 3 seconds
+    }
+};
