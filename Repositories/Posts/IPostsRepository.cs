849ff3241f02d1f8d014b457d56af3bf2a6abfc4
NoVacation <andersoncelestino.n@gmail.com>
2024-12-15 11:50:09 -0300

    GetPostsByUserId created. It also uses offset and page size to limit the return.

--- Repositories/Posts/IPostsRepository.cs
+++ Repositories/Posts/IPostsRepository.cs
@@ -1,8 +1,11 @@
 using BlackMetalBlog.Dtos.Posts;
+using BlackMetalBlog.Models.Entities;
 
 namespace BlackMetalBlog.Repositories.Posts;
 
 public interface IPostsRepository
 {
     Task CreatePost(CreatePostDto createPostDto);
+
+    Task<List<PostsEntity>> GetPostsByUserId(int userId, int offset, int pageSize);
 }
--- Repositories/Posts/PostsRepository.cs
+++ Repositories/Posts/PostsRepository.cs
@@ -1,5 +1,6 @@
 using BlackMetalBlog.Data;
 using BlackMetalBlog.Dtos.Posts;
+using BlackMetalBlog.Models.Entities;
 using Dapper;
 
 namespace BlackMetalBlog.Repositories.Posts;
@@ -13,5 +14,17 @@ public class PostsRepository(DapperDbConnectionFactory dbConnectionFactory) : IP
         const string sql = "insert into Posts (UserId, Title, Content) values (@UserId, @Title, @Content)";
 
         await conn.ExecuteAsync(sql, createPostDto);
+    }
+
+    public async Task<List<PostsEntity>> GetPostsByUserId(int userId, int offset, int pageSize)
+    {
+        using var conn = await dbConnectionFactory.CreateConnectionAsync();
+        const string sql =
+            "select * from Posts where UserId = @UserId order by CreatedAt desc offset @Offset rows fetch next @PageSize rows only";
+
+        var posts = await conn.QueryAsync<PostsEntity>(sql,
+            new { UserId = userId, Offset = offset, PageSize = pageSize });
+
+        return posts.ToList();
     }
 }
